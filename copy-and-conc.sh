0620ebe72ae5b7eb06ee0aec2fb08c9bd4c0246e
Mirza Krak <mirza.krak@northern.tech>
2019-03-10 22:51:09 +0100

    initial commit
    Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

---
+++ copy-and-conc.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+set -e
+
+# Utility script that converts "PNG" and "PDF" files in to a single TIFF file
+# with CCITT Group 4 (T.6) compression
+#
+# The input files are first converted to monochrome PBM files before they
+# are converted to TIFF
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -rf output
+mkdir ${script_dir}/output
+
+cp ${script_dir}/input/* ${script_dir}/output
+cd ${script_dir}/output
+
+cnt=0
+for f in *.{pdf,PDF}; do
+    if [ -f "${f}" ]; then
+        echo "converting $f to ${cnt}.pbm"
+        pdftoppm -mono "${f}" "${cnt}"
+        let "cnt += 1"
+    fi
+done
+for f in *.png; do
+    if [ -f "${f}" ]; then
+        echo "converting $f to ${cnt}.pbm"
+        convert "${f}" "${cnt}.pbm"
+        let "cnt += 1"
+    fi
+done
+
+for f in *.pbm; do
+    if [ -f "${f}" ]; then
+        echo "converting $f to ${cnt}.tiff"
+        convert "${f}" "${cnt}.pbm"
+        ppm2tiff -c g4 "${f}" "${cnt}.tiff"
+        let "cnt += 1"
+    fi
+done
+
+TIFF_FILES=$(ls *.tiff | tr "\n" " ")
+
+# Merge tiff`s if any
+if [ ! -z "${TIFF_FILES}" ]; then
+    echo "Running command to merge: "
+    echo "    tiffcp -c g4 ${TIFF_FILES} receipts.tiff"
+    tiffcp -c g4 ${TIFF_FILES} receipts.tiff
+fi
+
+echo "Conversion done: ${script_dir}/output/receipts.tiff"
+
+cd -
